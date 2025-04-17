709dd5e66de81d5a4ae92303d597ffe790391033
Esma <esma.alicic2002@gmail.com>
2025-04-17 15:08:53 +0200

    Removed ConfirmationPage.js and CartPage.js

--- test/pageobjects/ConfirmationPage.js
+++
@@ -1,9 +0,0 @@
-class ConfirmationPage {
-    get continueShoppingLink() { return $('//a[@class="action primary continue" and contains(@href, "magento.softwaretestingboard.com")]/span[text()="Continue Shopping"]'); }
-
-    async continueShopping() {
-        await this.continueShoppingLink.click();
-    }
-}
-
-export default new ConfirmationPage();
